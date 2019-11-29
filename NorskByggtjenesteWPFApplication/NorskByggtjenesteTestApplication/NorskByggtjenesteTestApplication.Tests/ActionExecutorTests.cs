8dc7a6891d905c13029d269548bd53f23c1aa436
moggieggwp <dmytro.hryhoriev@nure.ua>
2019-11-30 01:13:54 +0200

    Add WPF Project

---
+++ NorskByggtjenesteWPFApplication/NorskByggtjenesteTestApplication/NorskByggtjenesteTestApplication.Tests/ActionExecutorTests.cs
@@ -0,0 +1,118 @@
+﻿using NorskByggtjenesteTestApplication.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace NorskByggtjenesteTestApplication.Tests
+{
+    public class ActionExecutorTests
+    {
+        [Fact]
+        public void ActionsMustBeExecutedInBackground()
+        {
+            var executer = new ActionExecutor();
+
+            executer.AddActionToQueue(() =>
+            {
+                Thread.Sleep(Timeout.Infinite);
+            });
+
+            executer.Execute();
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ActionsMustBeExecutedSequentially()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var executer = new ActionExecutor();
+            executer.AddActionToQueue(() =>
+            {
+                stringBuilder.Append("My ");
+            });
+            executer.AddActionToQueue(() =>
+            {
+                stringBuilder.Append("name ");
+            });
+            executer.AddActionToQueue(() =>
+            {
+                stringBuilder.Append("is ");
+            });
+            executer.AddActionToQueue(() =>
+            {
+                stringBuilder.Append("Dmitriy");
+            });
+
+            var task = executer.Execute();
+            task.GetAwaiter().GetResult();
+
+            Assert.Equal("My name is Dmitriy", stringBuilder.ToString());
+        }
+
+        [Fact]
+        public void ActionsCanBeAddedNotAtTheSameTime()
+        {
+            int sum = 0;
+
+            var executer = new ActionExecutor();
+
+            sum += 5;
+
+            executer.AddActionToQueue(() =>
+            {
+                sum += 10;
+            });
+
+            sum += 5;
+
+            executer.AddActionToQueue(() =>
+            {
+                sum += 10;
+            });
+
+            var task = executer.Execute();
+            task.GetAwaiter().GetResult();
+
+            Assert.Equal(30, sum);
+        }
+
+        [Fact]
+        public void ActionsCanBeExecutedNotAtTheSameThread()
+        {
+            List<int> threadIds = new List<int>();
+
+            var executer = new ActionExecutor();
+
+            executer.AddActionToQueue(() =>
+            {
+                threadIds.Add(Thread.CurrentThread.ManagedThreadId);
+            });
+
+            executer.AddActionToQueue(() =>
+            {
+                threadIds.Add(Thread.CurrentThread.ManagedThreadId);
+            });
+
+            executer.AddActionToQueue(() =>
+            {
+                threadIds.Add(Thread.CurrentThread.ManagedThreadId);
+            });
+
+            executer.AddActionToQueue(() =>
+            {
+                threadIds.Add(Thread.CurrentThread.ManagedThreadId);
+            });
+
+            var task = executer.Execute();
+            task.GetAwaiter().GetResult();
+
+            var uniqueItems = threadIds.Distinct().Count();
+
+            Assert.True(uniqueItems >= 1);
+        }
+    }
+}
---
+++ NorskByggtjenesteWPFApplication/NorskByggtjenesteTestApplication/NorskByggtjenesteTestApplication/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using NorskByggtjenesteTestApplication.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace NorskByggtjenesteTestApplication
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> fileContents = new List<string>(); //List with file's content
+        private string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); //Path to Documents folder where file will be saved
+        private string fileNameTemplate = "generated_file_{0}.txt"; //Template of file name where {0} number of action in queue
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.CountOfContentsLabel.Content = "0"; //Default value of label
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var actionExecutor = new ActionExecutor(); //Initialization of ActionExecutor class
+
+            //Select each created content
+            fileContents.Select((content, index) =>
+            {
+                //Add action with writing content to file to list of actions
+                actionExecutor.AddActionToQueue(() =>
+                {
+                    Thread.Sleep(2000); //Just to make action longer and be sure that UI is not blocked
+                    //Standart .net procudere of writing file to path
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(documentsPath, string.Format(fileNameTemplate, ++index))))
+                    {
+                        outputFile.Write(content);
+                    }
+                });
+                return content;
+            }).ToList(); //Because .Select() returns IEnumerable and it's causes LazyLoading
+
+            actionExecutor.Execute(); //Execute actions in background sequentially
+
+            this.ContentTextBox.Clear(); //Refresh TextBox
+            this.CountOfContentsLabel.Content = "0"; //Default value of label
+        }
+
+        private void AddNewContent_Click(object sender, RoutedEventArgs e)
+        {
+            fileContents.Add(this.ContentTextBox.Text); //Add content from TextBox to list of contents
+
+            this.CountOfContentsLabel.Content = fileContents.Count; //Show label with count of contents
+            this.ContentTextBox.Clear(); //Refresh TextBox
+        }
+    }
+}
